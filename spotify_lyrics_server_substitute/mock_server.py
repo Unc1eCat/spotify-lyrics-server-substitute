1a595b750191cd38639d80887d3c1b984e5f1609
Kanye West <antonbesp.25@gmail.com>
2024-04-20 15:05:28 +0300

    Wrote rough conversion of lyrics to JSON response, not tested yet

--- spotify_lyrics_server_substitute/mock_server.py
+++ spotify_lyrics_server_substitute/mock_server.py
@@ -1,22 +1,43 @@
+import json
 import re
 from http.server import BaseHTTPRequestHandler
 
-from spotify_api import SpotifyAPI
+from spotify_lyrics_server_substitute.lyrics_backends.lyrics_backend import LyricsBackendBase
+from spotify_lyrics_server_substitute import spotify_api
 
 LYRICS_URL_TRACK_PART = re.compile(r"track/([a-zA-Z0-9]+)/image")
 
 
-def lyrics_request_handler_class_factory(spotify_api: str): # Used to DI things into the request handler 
-    return type('LyricsRequestHandler', (_LyricsRequestHandlerBase,), {"spotify_api": spotify_api})
+class LyricsRequestHandler(BaseHTTPRequestHandler):
+    backends: list[LyricsBackendBase]
 
-class _LyricsRequestHandlerBase(BaseHTTPRequestHandler):
+    @staticmethod
+    def as_class(backends: list[LyricsBackendBase]):
+        return type('LyricsRequestHandlerSub', (LyricsRequestHandler,), {
+            'backends': backends
+        })
+
+    def assert_factory_used(self):
+        assert self.__class__.__name__ == 'LyricsRequestHandlerSub', "You can't use `LyricsRequestHandler class directly, please use `LyricsRequestHandler.as_class` static factory."
+
     def get_track_id(self):
         try:
-            return LYRICS_URL_TRACK_PART.search(self.path)[0]
-        except IndexError:
+            return LYRICS_URL_TRACK_PART.search(self.path)[1]
+        except (IndexError, AttributeError):
             print("A request was received from the Spotify client that cannot be handled.")
             raise
 
     def do_GET(self):
-        track_id = self.get_track_id()
+        self.assert_factory_used()
 
+        track_id = self.get_track_id()
+        for i in self.backends:
+            if lyrics := i.get_lyrics(track_id):
+                break
+        else:  # None of the backends found lyrics
+            ...
+        # A backend found lyrics
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps(spotify_api.make_response_body(lyrics)))
