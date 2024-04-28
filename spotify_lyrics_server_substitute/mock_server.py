d013e874454de67e7772a1321b1bd5de3fd07281
Kanye West <antonbesp.25@gmail.com>
2024-04-29 01:35:53 +0300

    FOR GOD MAKE THE FOWARDING USE SMTHING ELSE THAN THAT CRAPPY URLLIB

--- spotify_lyrics_server_substitute/mock_server.py
+++ spotify_lyrics_server_substitute/mock_server.py
@@ -1,9 +1,11 @@
 from collections import namedtuple
+import gzip
 import json
 import re
 from http.server import BaseHTTPRequestHandler
 import socket
 from typing import Iterable
+from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
 from spotify_lyrics_server_substitute.lyrics_backends.lyrics_backend import LyricsBackendBase
@@ -27,20 +29,31 @@ class LyricsRequestHandler(BaseHTTPRequestHandler):
         ''' Checks that the client does not directly use `LyricsRequestHandler` class and instead has called `as_class` static factory. '''
         assert self.__class__.__name__ == 'LyricsRequestHandlerSub', "You can't use `LyricsRequestHandler class directly, please use `LyricsRequestHandler.as_class` static factory."
 
+    # TODO: Make a strategy for this method. Turns out forwarding HTTP requests is not as easy as I thought
     def forward_to_real_server(self):
         ''' Sends the received request to real Spotify servers and sends the response they give back to the client app. '''
         # TODO: Rewrite it. Make it use TLS sockets and send contents of `self.rfile` through them to the real servers
-        req = Request('https://%s' % self.headers['host'] + self.path, data=self.rfile.read(), headers=dict(self.headers), method=self.command.upper())
-        with urlopen(req) as res:
-            self.send_response(res.code)
-            for k, v in dict(res.headers).items():
-                self.send_header(k, v)
-            self.end_headers()
-            self.wfile.write(res.read())
+        url = 'https://%s' % (self.headers['host'] + self.path,)
+        content_length = int(self.headers.get('Content-Length', 0))
+        req = Request(url, data=self.rfile.read(content_length), headers=dict(self.headers), method=self.command.upper())
+        try:
+            with urlopen(req) as res:
+                self.send_response(res.code)
+                for k, v in dict(res.headers).items():
+                    self.send_header(k, v)
+                # self.send_header('Content-Length', len(r := res.read()))
+                self.end_headers()
+                # content_length = int(res.headers.get('Content-Length', 0))
+                # print(gzip.decompress(res.read()))
+                # self.write(r)
+                self.wfile.write(res.read(content_length) if content_length else res.read())
+        except HTTPError as e:
+            self.send_error(e.getcode(), e.reason)
 
     def extract_from_request(self):
         ''' Returns only the information we need from the request or None if the request should be forwarded to the real Spotify servers. '''
         try:
+            return None
             match = LYRICS_URL_PATTERN.search(self.path)
             return ParsedRequest(match[1], match[2])
         except (IndexError, TypeError):
@@ -59,6 +72,7 @@ class LyricsRequestHandler(BaseHTTPRequestHandler):
                 break
         else:  # None of the backends found lyrics
             ...
+            return
         # A backend found lyrics
 
         self.send_response(200)
